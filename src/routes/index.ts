7366ffa26cadcf978bc2bfd26e07649d08d9a672
Gabriel <gabrielvduarte9@gmail.com>
2026-06-13 10:26:47 -0300

    Autenticação e autorização finalizadas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { usersRoutes } from "./users-routes.js";
 import { sessionsRoutes } from "./sessions-routes.js";
 import { refundsRoutes } from "./refunds-routes.js";
+import { ensureAuhenticated } from "@/middlewares/ensure-authentication.js";
 
 const routes = Router()
 
 routes.use("/users",usersRoutes)
 routes.use("/sessions",sessionsRoutes)
 //Rotas privadas
+routes.use(ensureAuhenticated)
 routes.use("/refunds",refundsRoutes)
 
 export {routes}
