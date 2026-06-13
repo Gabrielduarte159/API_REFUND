791956f51008a5089cc832aa45392300adce131d
Gabriel <gabrielvduarte9@gmail.com>
2026-06-13 11:47:21 -0300

    demais funcionalidades pt1

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,4 +1,4 @@
-import { Request,Response } from "express"
+import { query, Request,Response } from "express"
 import { AppError } from "@/utils/AppError.js"
 import { authConfig } from "@/configs/auth.js"
 import { prisma } from "@/database/prisma.js"
@@ -6,9 +6,54 @@ import jwt from "jsonwebtoken"
 import { compare } from "bcrypt"
 import {z} from "zod"
 
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation",
+])
+
 class RefundsController{
   async create(request:Request,response:Response){
-    response.json({message:"ok"})
+    const bodySchema = z.object({
+      name:z.string().trim().min(1,{message:"Informe o nome da solicitação"}),
+      category:CategoriesEnum,
+      amount:z.number().positive({message:"O valor precisa ser positivo"}),
+      filename: z.string().min(20)
+    })
+    const {name,category,amount,filename} = bodySchema.parse(request.body)
+    if(!request.user?.id){
+      throw new AppError("Unauthorized",401)
+    }
+    const refund = await prisma.refunds.create({
+      data:{
+        name,
+        category,
+        amount,
+        filename,
+        userId:request.user?.id,
+      },
+    })
+    response.status(201).json(refund)
+  }
+  async index(request:Request,response:Response){
+   const querySchema = z.object({
+    name:z.string().optional().default(""),
+   })
+   const {name} = querySchema.parse(request.query)
+   const refunds = await prisma.refunds.findMany({
+    where:{
+      user:{
+        name:{
+          contains:name.trim(),
+        },
+      },
+    },
+    orderBy:{createdAt:"desc"},
+    include: {user:true}
+   })
+   response.json(refunds)
   }
 }
 
